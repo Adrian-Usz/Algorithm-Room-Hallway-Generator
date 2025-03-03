155875fc4ece6b82b8bdf18e749ba3e5c3e97271
Adrian-Usz <adrianuszynski637@gmail.com>
2025-03-03 12:12:48 -0500

    Added simple Hallway function and began working on the functionality of it.

--- simple-map-gen/main.cpp
+++ simple-map-gen/main.cpp
@@ -3,40 +3,77 @@
 
 using namespace std;
 
-
 //! This starts from the center, so if its 50, 50, then it will increase both to 51 and start from 25,25
-struct Room {
+struct Room
+{
     size_t xStart, yStart, width, height;
 };
 
-size_t hallWidth = 1; //Width of the hallways
+size_t hallWidth = 1; // Width of the hallways
 
-char wallChar = 'W'; //Character for the walls
-char floorChar = 'F'; //Character for the floor
+char wallChar = 'W';  // Character for the walls
+char floorChar = '#'; // Character for the floor
 //! Rooms start at the X, Y defined and go to the right and down from that point to generate that room
 
+
+
+
+void hallwaySimpleGenerator(size_t row, size_t col, bool touchBorderEnd, int setHallwayCount);
 void rpgSimpleGenerator(size_t row, size_t col);
 
-
 int main()
-{   
+{
+    //! The boolean value makes it so that if the hallway touches the border wall, it will end there. Else it will continue and use setHallwayCount to determine how many hallways to generate; 
     size_t rpgRow = 50;
     size_t rpgCol = 50;
-    rpgSimpleGenerator(rpgRow, rpgCol);    
+    int setHallWayCount = 3;
+
+    hallwaySimpleGenerator(rpgRow, rpgCol, false, setHallWayCount);
+    //rpgSimpleGenerator(rpgRow, rpgCol);
+
 
     return 0;
 }
+//! This is a simple hallway generator, it will create hallways, starting from the center of the map
 
-void rpgSimpleGenerator(size_t row, size_t col){
+void hallwaySimpleGenerator(size_t row, size_t col, bool touchBorderEnd, int setHallwayCount)
+{
+    int randomDirectionAmount;
+    int turnDirection;
+
+
+
     if (row % 2 == 0)
     {
         row += 1;
-        //cout << ROW << endl; //*Need to remove later
     }
     if (col % 2 == 0)
     {
         col += 1;
-        //cout << COL << endl; //*Need to remove later
+    }
+
+    while (true)
+    {
+        vector<vector<char>> grid(row, vector<char>(col));
+
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+            }
+        }
+    }
+}
+
+void rpgSimpleGenerator(size_t row, size_t col)
+{
+    if (row % 2 == 0)
+    {
+        row += 1;
+    }
+    if (col % 2 == 0)
+    {
+        col += 1;
     }
     vector<vector<char>> grid(row, vector<char>(col));
 }
